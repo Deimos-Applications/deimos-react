63ccb2b7e156ef817b6bcccc81b07e11c9beca3a
Brandon G. Neri <arcaniteamp@gmail.com>
2020-08-13 11:29:51 -0500

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <div className={styles["deimos-react"]}>
+      <header>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/arcanite24/deimos-react"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Deimos React
         </a>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Testing TailwindCSS
+        </button>
       </header>
     </div>
   );
